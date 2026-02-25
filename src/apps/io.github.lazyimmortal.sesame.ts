9e22f1a50e47975ddc9731b903930bd444548c01
刘帅 <liu.5431@qq.com>
2026-02-25 10:07:32 +0800

    feat: 芝麻粒-GR

---
+++ src/apps/io.github.lazyimmortal.sesame.ts
@@ -0,0 +1,21 @@
+import { defineGkdApp } from '@gkd-kit/define';
+
+export default defineGkdApp({
+  id: 'io.github.lazyimmortal.sesame',
+  name: '芝麻粒-GR',
+  groups: [
+    {
+      key: 1,
+      name: '跳过提示',
+      desc: '点击[我知道了]',
+      rules: [
+        {
+          fastQuery: true,
+          activityIds: 'io.github.lazyimmortal.sesame.ui.MainActivity',
+          matches: 'Button[id="android:id/button1"]',
+          snapshotUrls: 'https://i.gkd.li/i/25533841',
+        },
+      ],
+    },
+  ],
+});
